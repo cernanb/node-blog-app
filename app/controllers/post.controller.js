efbd0cd56369493a152be151e7ab9e83446b7cc5
Luke Ghenco <lukeghenco@gmail.com>
2016-12-14 15:53:24 -0500

    post_controller and routes are functional

--- app/controllers/post.controller.js
+++ app/controllers/post.controller.js
@@ -4,10 +4,27 @@ const postRouter  = express.Router()
 
 postRouter
     .get('/', (req, res) => {
-        res.render('post/index', { header: 'post index'} )
+        res.render('post/index', { header: 'post index' })
     })
     .get('/new', (req, res) => {
-        res.render('post/new', { header: 'post new' } )
+        res.render('post/new', { header: 'post new' })
+    })
+    .get('/:id', (req, res) => {
+        res.render('post/show', { header: 'post show', post: { id: 1, title: '1st Post', description: 'lorem ipsum' } })
+    })
+    .post('/', (req, res) => {
+        console.log(req.body)
+        res.redirect('/posts/1')
+    })
+    .get('/:id/edit', (req, res) => {
+        res.render('post/edit', { header: 'post edit', post: { id: 1, title: '1st Post', description: 'lorem ipsum' } })
+    })
+    .put('/:id', (req, res) => {
+        console.log(req.body)
+        res.redirect('/posts/1')
+    })
+    .delete('/:id', (req, res) => {
+        console.log('deleting stuff')
     })
 
 module.exports = postRouter
